8874e1f251b5860a74e9e228c6d4f909afc1fde4
paruls21 <36991374+paruls21@users.noreply.github.com>
2018-04-06 19:06:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function makeGrid() {
-	var canvas, cell, gridHeight, gridWidth, rows;
-	canvas = $('#pixel_canvas');
-	gridHeight = $('#input_height').val();
-	gridWidth = $('#input_width').val();
-	canvas.children().remove()
-	for (a=0;a<gridHeight;a++) {
-	canvas.append('<tr></tr>');
-	}
-		rows = $('tr');
-	for (y=0;y<gridWidth;y++) {
-	rows.append('<td></td>');
-	} 
-	cell = canvas.find('td');
-	cell.click(function() {
-		var color;
-		color = $("#qwertyc").val();
-		$(this).attr('bgcolor', color);
-	});	
+  var canvas, cell, gridHeight, gridWidth, rows;
+  canvas = $("#pixel_canvas");
+  gridHeight = $("#input_height").val();
+  gridWidth = $("#input_width").val();
+  canvas.children().remove();
+  for (a = 0; a < gridHeight; a++) {
+    canvas.append("<tr></tr>");
+  }
+  rows = $("tr");
+  for (y = 0; y < gridWidth; y++) {
+    rows.append("<td></td>");
+  }
+  cell = canvas.find("td");
+  cell.click(function() {
+    var color;
+    color = $("#qwertyc").val();
+    $(this).attr("bgcolor", color);
+  });
 }
 var submitQuery;
 submitQuery = $('input[type="submit"]')
