03c37be37b8379841dd59538cfc7baab7e96bc1f
paruls21 <36991374+paruls21@users.noreply.github.com>
2018-04-08 22:49:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function makeGrid() {
-  var canvas, cell, gridHeight, gridWidth, rows;
+  var canvas, cell, gridHeight, gridWidth, rows;//select size
   canvas = $("#pixel_canvas");
-  gridHeight = $("#input_height").val();
-  gridWidth = $("#input_width").val();
+  gridHeight = $("#input_height").val();//value of height
+  gridWidth = $("#input_width").val();//value of width
   canvas.children().remove();
   for (a = 0; a < gridHeight; a++) {
-    canvas.append("<tr></tr>");
+    canvas.append("<tr></tr>");//after selection of total rows nd columns rows are made here
   }
   rows = $("tr");
   for (y = 0; y < gridWidth; y++) {
-    rows.append("<td></td>");
+    rows.append("<td></td>");//after selection of total rows nd columns column are made here
   }
   cell = canvas.find("td");
+  //when user click it change the color
   cell.click(function() {
-    var color;
+    var color;//select color
     color = $("#qwertyc").val();
     $(this).attr("bgcolor", color);
   });
 }
-var submitQuery;
+var submitQuery;// When the user submit the size then call function makeGrid()
 submitQuery = $('input[type="submit"]')
 submitQuery.click(function(event) {
   event.preventDefault();
